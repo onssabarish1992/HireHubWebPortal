c647c1048da74c923b05f2baf0e0f6b4daad759c
Sabarish Nair <121101278@umail.ucc.ie>
2022-05-13 17:55:48 +0100

    Login and Register Page
    Login and Register Page

--- HRAnalytics.Web/Program.cs
+++ HRAnalytics.Web/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HRAnalytics.Web.Data;
+using HRAnalytics.Web.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AuthContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +30,35 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+               name: "default",
+               pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapRazorPages();
+});
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var RoleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roleNames = { "HR", "Interviewer" };
+    IdentityResult roleResult;
+
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await RoleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            //create the roles and seed them to the database: Question 1
+            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
 
 app.Run();
