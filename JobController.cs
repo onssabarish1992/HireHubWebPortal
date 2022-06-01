08b68746f16d785ff4a669e4a350db4602c93811
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-06-01 19:47:50 +0100

    Add files via upload

---
+++ JobController.cs
@@ -0,0 +1,146 @@
+﻿using HRAnalytics.BL.Interfaces;
+using HRAnalytics.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRAnalytics.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        IJobBL _jobBL;
+
+        public JobController(IJobBL jobBL)
+        {
+            _jobBL = jobBL;
+        }
+
+        [HttpGet]
+        [Route("GetJobs")]
+        public IActionResult GetJobs()
+        {
+            #region Declarations
+            JobCollection l_jobCollection;
+            #endregion
+            try
+            {
+                l_jobCollection = _jobBL.GetAllJobs();
+
+                if (l_jobCollection == null || l_jobCollection.Count == 0)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(l_jobCollection);
+        }
+
+        [HttpGet]
+        [Route("GetEvaluationCriteria")]
+        public IActionResult GetEvaluationCriteria()
+        {
+            #region Declarations
+            JobCollection l_jobCollection;
+            #endregion
+            try
+            {
+                l_jobCollection = _jobBL.GetEvaluationCriteria();
+
+                if (l_jobCollection == null || l_jobCollection.Count == 0)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(l_jobCollection);
+        }
+        [HttpPost]
+        [Route("SaveSubCriteria")]
+        public IActionResult SaveSubCriteria(string LoggedInUser, [FromBody] Job job)
+        {
+            try
+            {
+                _jobBL.SaveSubCriteria(LoggedInUser, job);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok();
+        }
+        [HttpGet]
+        [Route("GetCriteriaForJob")]
+        public IActionResult GetCriteriaForJob(int argJobId)
+        {
+            #region Declarations
+            JobCollection l_jobCollection;
+            #endregion
+            try
+            {
+                l_jobCollection = _jobBL.GetCriteriaForJob(argJobId);
+
+                if (l_jobCollection == null || l_jobCollection.Count == 0)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(l_jobCollection);
+        }
+
+        [HttpGet]
+        [Route("GetRoles")]
+        public IActionResult GetRoles()
+        {
+            #region Declarations
+            JobCollection l_jobCollection;
+            #endregion
+            try
+            {
+                l_jobCollection = _jobBL.GetAllJobs();
+
+                if (l_jobCollection == null || l_jobCollection.Count == 0)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(l_jobCollection);
+        }
+
+        [HttpPost]
+        [Route("SaveRole")]
+        public IActionResult SaveRole(string LoggedInUser, [FromBody] Job job)
+        {
+            try
+            {
+                _jobBL.SaveRole(LoggedInUser, job);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok();
+        }
+    }
+
+}
