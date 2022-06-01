aa8b483e84941aa6dc3f37edb721fb696758c2a1
KishoreValavan <47786480+KishoreValavan@users.noreply.github.com>
2022-06-01 19:50:11 +0100

    Add files via upload

--- HRAnalytics.BL/JobBL.cs
+++ HRAnalytics.BL/JobBL.cs
@@ -169,5 +169,71 @@ namespace HRAnalytics.BL
 
 
         }
+
+        public JobCollection GetRoles()
+        {
+            #region Declarations
+            JobCollection l_JobCollection = new();
+            HRAnalyticsDBManager l_HRAnalyticsDBManager = new("HRAnalyticsConnection", _configuration);
+            DataTable l_JobDataTable;
+            Job l_Job;
+            int l_JobCount;
+            #endregion
+            try
+            {
+                l_JobDataTable = l_HRAnalyticsDBManager.GetDataTable(StoredProcedure.GET_JOBCRITERIA, CommandType.StoredProcedure);
+
+                if (l_JobDataTable != null && l_JobDataTable.Rows.Count > 0)
+                {
+                    l_JobCount = l_JobDataTable.Rows.Count;
+                    for (int i = 0; i < l_JobCount; i++)
+                    {
+                        l_Job = new Job();
+
+                        DataRow l_Row = l_JobDataTable.Rows[i];
+
+                        l_Job.JobId = l_Row["job_id"] == DBNull.Value ? 0 : Convert.ToInt32(l_Row["job_id"]);
+                        l_Job.Position = l_Row["position_count"] == DBNull.Value ? 0 : Convert.ToInt32(l_Row["position"]);
+                        l_Job.Compensation = l_Row["compensation"] == DBNull.Value ? 0 : Convert.ToDouble(l_Row["compensation"]);
+                        l_Job.JobDescription = l_Row["job_description"] == DBNull.Value ? string.Empty : Convert.ToString(l_Row["job_description"]);
+                        l_JobCollection.Add(l_Job);
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return l_JobCollection;
+        }
+
+        public void SaveRole(string argLoggedInUser, Job argJob)
+        {
+            #region Declarations
+            HRAnalyticsDBManager l_HRAnalyticsDBManager = new("HRAnalyticsConnection", _configuration);
+            List<IDbDataParameter> l_Parameters = new();
+            int l_LastID = 0;
+            #endregion
+            try
+            {
+                if (argJob != null)
+                {
+                    //Create Parameters
+                    l_Parameters.Add(l_HRAnalyticsDBManager.CreateParameter(ProcedureParams.JOBID, argJob.JobId, DbType.Int32));
+                    l_Parameters.Add(l_HRAnalyticsDBManager.CreateParameter(ProcedureParams.POSITION, argJob.Position, DbType.Int32));
+                    l_Parameters.Add(l_HRAnalyticsDBManager.CreateParameter(ProcedureParams.COMPENSATOIN, argJob.Compensation, DbType.Double));
+                    l_Parameters.Add(l_HRAnalyticsDBManager.CreateParameter(ProcedureParams.JOBDESCRITPTION, argLoggedInUser, DbType.String));
+
+                    //Call stored procedure
+                    l_HRAnalyticsDBManager.Insert(StoredProcedure.SET_JOBCRITERIA, CommandType.StoredProcedure, l_Parameters.ToArray(), out l_LastID);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
     }
 }
