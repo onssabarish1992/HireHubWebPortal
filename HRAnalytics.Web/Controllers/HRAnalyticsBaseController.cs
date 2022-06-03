24db456b5c455ef95cdbb87c1aa3509a19a99f59
Sabarish Nair <121101278@umail.ucc.ie>
2022-06-03 21:10:47 +0100

    SubCriteria page
    SubCriteria page

--- HRAnalytics.Web/Controllers/HRAnalyticsBaseController.cs
+++ HRAnalytics.Web/Controllers/HRAnalyticsBaseController.cs
@@ -68,5 +68,34 @@ namespace HRAnalytics.Web.Controllers
             }
             return l_JobCollection;
         }
+
+        /// <summary>
+        /// API call to get the job criterias created so far
+        /// </summary>
+        /// <returns></returns>
+        public async Task<JobCollection> GetJobCriterias()
+        {
+            #region Declarations
+            JobCollection l_JobCollection = new();
+            string l_jobURL = string.Empty;
+            #endregion
+            try
+            {
+                l_jobURL = apiBaseURL + "api/Job/GetRoles";
+                HttpResponseMessage l_jobData = await client.GetAsync(l_jobURL);
+
+                if (l_jobData != null && l_jobData.IsSuccessStatusCode)
+                {
+                    var l_JobResponse = l_jobData.Content.ReadAsStringAsync().Result;
+                    l_JobCollection = JsonConvert.DeserializeObject<JobCollection>(l_JobResponse);
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return l_JobCollection;
+        }
     }
 }
---
+++ HRAnalytics.Web/Models/SubcriteriaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRAnalytics.Web.Models
+{
+    public class SubcriteriaViewModel
+    {
+        [Required(ErrorMessage = "Please select Job Role")]
+        public int JobId { get; set; }
+
+        [Required(ErrorMessage = "Please select subcriteria")]
+        public int CriteriaId { get; set; }
+
+        [Required(ErrorMessage = "Please enter description")]
+        public string CriteriaDescription { get; set; }
+        public string? DisplayCriterias { get; set; }
+        public string? JobName { get; set; }
+        public string? CriteriaName { get; set; }
+        public int? CriteriaCount { get; set; }
+    }
+
+    public class CriteriaViewModel
+    {
+        public SubcriteriaViewModel? Criteria { get; set; }
+
+        public List<SubcriteriaViewModel>? CriteriasCreated { get; set; }
+    }
+}
