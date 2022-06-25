44ad4a264e387d20473902573dfc6bc0ce43c7be
Sabarish Nair <121101278@umail.ucc.ie>
2022-06-25 21:01:23 +0100

    UI Changes
    UI Changes

---
+++ HRAnalytics.Web/Controllers/TopsisResults.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRAnalytics.Web.Controllers
+{
+    public class TopsisResults : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
